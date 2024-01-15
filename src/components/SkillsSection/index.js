987c95c65eb9d6adb3f62b182ab59f1e256d0475
Manas Patil <patilmanas1304@gmail.com>
2024-01-15 18:51:44 +0530

    Updated the skills

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -125,7 +125,7 @@ const SkillsSection = ()=>{
             <Wrapper>
                 <Title>Skills</Title>
                 <Desc>
-                    Here are some of my skills on which I have been working on for the past 2 years.
+                    Here are my code superpowers which I have working on for the past 2 years.
                 </Desc>
                 <SkillsContainer>
                     {
